d43c86a55de4c1cc32bb6d24e2e15d955b432526
caspervanneck <nethercube.host@gmail.com>
2016-10-31 09:19:37 +0100

    fix rakib block 5 seconds
    you can now join ImagicalMine

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -36,6 +36,7 @@ interface Info{
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 91;
+	const ACCEPTED_PROTOCOLS = [90, 91];
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
